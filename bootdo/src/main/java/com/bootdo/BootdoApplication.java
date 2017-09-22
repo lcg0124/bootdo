baa9532bad656929c3cea239473d21b7722f1f25
chguang lee <1992lcg@163.com>
2017-09-22 11:20:26 +0800

    修复druid连接池sql不能显示的bug

--- bootdo/src/main/java/com/bootdo/BootdoApplication.java
+++ bootdo/src/main/java/com/bootdo/BootdoApplication.java
@@ -1,18 +1,17 @@
 package com.bootdo;
 
-import javax.servlet.MultipartConfigElement;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
+@ServletComponentScan 
 @MapperScan("com.bootdo.*.dao")
 @SpringBootApplication
 public class BootdoApplication {
@@ -37,5 +36,4 @@ public class BootdoApplication {
 
 	}
 
-	
 }
