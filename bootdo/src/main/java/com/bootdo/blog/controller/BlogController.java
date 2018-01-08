571c3a983109912264116b124bba9a9cd05f7ffa
lcg0124 <1992lcg@163.com>
2018-01-08 11:04:13 +0800

    修复新增加模型，列表不刷新的bug

--- bootdo/src/main/java/com/bootdo/blog/controller/BlogController.java
+++ bootdo/src/main/java/com/bootdo/blog/controller/BlogController.java
@@ -14,6 +14,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author bootdo 1992lcg@163.com
+ */
 @RequestMapping("/blog")
 @Controller
 public class BlogController {
@@ -28,14 +31,10 @@ public class BlogController {
 	@ResponseBody
 	@GetMapping("/open/list")
 	public PageUtils opentList(@RequestParam Map<String, Object> params) {
-		// 查询列表数据
 		Query query = new Query(params);
-
 		List<ContentDO> bContentList = bContentService.list(query);
 		int total = bContentService.count(query);
-
 		PageUtils pageUtils = new PageUtils(bContentList, total);
-
 		return pageUtils;
 	}
 
@@ -50,7 +49,10 @@ public class BlogController {
 	String about(@PathVariable("categories") String categories, Model model) {
 		Map<String, Object> map = new HashMap<>(16);
 		map.put("categories", categories);
-		ContentDO bContentDO = bContentService.list(map).get(0);
+		ContentDO bContentDO =null;
+		if(bContentService.list(map).size()>0){
+			 bContentDO = bContentService.list(map).get(0);
+		}
 		model.addAttribute("bContent", bContentDO);
 		return "blog/index/post";
 	}
