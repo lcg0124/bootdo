5555ae432b35afc4673efcf8438e6e887f281f11
BeautifulHao <78714011@qq.com>
2018-03-18 15:26:40 +0800

    调整

--- bootdo/src/main/java/com/bootdo/BootdoApplication.java
+++ bootdo/src/main/java/com/bootdo/BootdoApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ServletComponentScan
