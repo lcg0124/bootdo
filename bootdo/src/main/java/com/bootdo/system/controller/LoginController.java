5c06a095a1200c269580caedd6ee8132a94f8f5a
lcg0124 <1992lcg@163.com>
2018-11-16 18:17:44 +0800

    1.把登录默认的用户名放在配置文件中,区分不同环境

--- bootdo/src/main/java/com/bootdo/system/controller/LoginController.java
+++ bootdo/src/main/java/com/bootdo/system/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.bootdo.system.controller;
 
 import com.bootdo.common.annotation.Log;
+import com.bootdo.common.config.BootdoConfig;
 import com.bootdo.common.controller.BaseController;
 import com.bootdo.common.domain.FileDO;
 import com.bootdo.common.domain.Tree;
@@ -28,68 +29,73 @@ import java.util.List;
 
 @Controller
 public class LoginController extends BaseController {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	MenuService menuService;
-	@Autowired
-	FileService fileService;
-	@GetMapping({ "/", "" })
-	String welcome(Model model) {
+    @Autowired
+    MenuService menuService;
+    @Autowired
+    FileService fileService;
+    @Autowired
+    BootdoConfig bootdoConfig;
 
-		return "redirect:/blog";
-	}
+    @GetMapping({"/", ""})
+    String welcome(Model model) {
 
-	@Log("请求访问主页")
-	@GetMapping({ "/index" })
-	String index(Model model) {
-		List<Tree<MenuDO>> menus = menuService.listMenuTree(getUserId());
-		model.addAttribute("menus", menus);
-		model.addAttribute("name", getUser().getName());
-		FileDO fileDO = fileService.get(getUser().getPicId());
-		if(fileDO!=null&&fileDO.getUrl()!=null){
-			if(fileService.isExist(fileDO.getUrl())){
-				model.addAttribute("picUrl",fileDO.getUrl());
-			}else {
-				model.addAttribute("picUrl","/img/photo_s.jpg");
-			}
-		}else {
-			model.addAttribute("picUrl","/img/photo_s.jpg");
-		}
-		model.addAttribute("username", getUser().getUsername());
-		return "index_v1";
-	}
+        return "redirect:/blog";
+    }
 
-	@GetMapping("/login")
-	String login() {
-		return "login";
-	}
+    @Log("请求访问主页")
+    @GetMapping({"/index"})
+    String index(Model model) {
+        List<Tree<MenuDO>> menus = menuService.listMenuTree(getUserId());
+        model.addAttribute("menus", menus);
+        model.addAttribute("name", getUser().getName());
+        FileDO fileDO = fileService.get(getUser().getPicId());
+        if (fileDO != null && fileDO.getUrl() != null) {
+            if (fileService.isExist(fileDO.getUrl())) {
+                model.addAttribute("picUrl", fileDO.getUrl());
+            } else {
+                model.addAttribute("picUrl", "/img/photo_s.jpg");
+            }
+        } else {
+            model.addAttribute("picUrl", "/img/photo_s.jpg");
+        }
+        model.addAttribute("username", getUser().getUsername());
+        return "index_v1";
+    }
 
-	@Log("登录")
-	@PostMapping("/login")
-	@ResponseBody
-	R ajaxLogin(String username, String password) {
+    @GetMapping("/login")
+    String login(Model model) {
+        model.addAttribute("username", bootdoConfig.getUsername());
+        model.addAttribute("password", bootdoConfig.getPassword());
+        return "login";
+    }
 
-		password = MD5Utils.encrypt(username, password);
-		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-		Subject subject = SecurityUtils.getSubject();
-		try {
-			subject.login(token);
-			return R.ok();
-		} catch (AuthenticationException e) {
-			return R.error("用户或密码错误");
-		}
-	}
+    @Log("登录")
+    @PostMapping("/login")
+    @ResponseBody
+    R ajaxLogin(String username, String password) {
 
-	@GetMapping("/logout")
-	String logout() {
-		ShiroUtils.logout();
-		return "redirect:/login";
-	}
+        password = MD5Utils.encrypt(username, password);
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(token);
+            return R.ok();
+        } catch (AuthenticationException e) {
+            return R.error("用户或密码错误");
+        }
+    }
 
-	@GetMapping("/main")
-	String main() {
-		return "main";
-	}
+    @GetMapping("/logout")
+    String logout() {
+        ShiroUtils.logout();
+        return "redirect:/login";
+    }
+
+    @GetMapping("/main")
+    String main() {
+        return "main";
+    }
 
 }
